c28379e6c5de3178365e1620ac81aded6f7eb30b
Ishtar <90619818+Ishtarcth@users.noreply.github.com>
2022-12-28 00:44:48 +0700

    reset

--- tools/main.js
+++
@@ -1,9 +0,0 @@
-//trieu scrollupbutton
-const btnScrollToTop = document.querySelector("#btnScrollToTop");
-btnScrollToTop.addEventListener("click", function () {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-    });
-});
