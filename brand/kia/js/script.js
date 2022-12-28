43afbd55106388435b675021be26947ec5152392
Ishtar <90619818+Ishtarcth@users.noreply.github.com>
2022-12-28 19:07:20 +0700

    major 6

---
+++ brand/kia/js/script.js
@@ -0,0 +1,8 @@
+const btnScrollToTop = document.querySelector("#btnScrollToTop");
+btnScrollToTop.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+});
